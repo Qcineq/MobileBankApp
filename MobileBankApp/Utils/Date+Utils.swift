64ee04b259932cfd56dd1e5615581fe09db2f70d
Kuba <jakub.kucinski@icloud.com>
2024-02-23 23:36:26 +0100

    Add networking to AccountSummary

---
+++ MobileBankApp/Utils/Date+Utils.swift
@@ -0,0 +1,22 @@
+//
+//  Date+Utils.swift
+//  MobileBankApp
+//
+//  Created by Jakub Kuciński on 23/02/2024.
+//
+
+import Foundation
+
+extension Date {
+    static var bankAppDateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.timeZone = TimeZone(abbreviation: "CET")
+        return formatter
+    }
+    
+    var dayMonthYearString: String {
+        let dateFormatter = Date.bankAppDateFormatter
+        dateFormatter.dateFormat = "dd.MM.yy"
+        return dateFormatter.string(from: self)
+    }
+}
